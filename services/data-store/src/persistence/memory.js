cacac7b394beccfd5cba8de751a79b51c6d2145a
eva <squidstock@hotmail.com>
2016-11-29 16:25:21 +0100

    gauge storage size

--- services/data-store/src/persistence/memory.js
+++ services/data-store/src/persistence/memory.js
@@ -3,9 +3,12 @@ module.exports = {
 };
 
 const storage = {};
+const metrics = require('microservice-toolkit').metrics;
 
 function save( id, article ){
     storage[id] = article;
+
+    metrics.gauge('storage-size', Object.keys(storage).length);
 
     return Promise.resolve();
 }
