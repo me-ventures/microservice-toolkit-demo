bc64c67981dc89917a1b13c4fd211e7659742ad1
M. Hofman <m.hofman@chronos-technology.nl>
2016-12-01 18:26:59 +0100

    Update infrastructure with monitoring

--- build-images.sh
+++ build-images.sh
@@ -1,27 +1,32 @@
+#!/bin/bash
+
+# Fail on error
+set -e
+
 cd services/article-aggregate
 npm install
-docker build -t article-aggregate:1.0 .
+docker build -t article-aggregate:1.1 .
 
 cd ../bbc-scraper
 npm install
-docker build -t bbc-scraper:1.0 .
+docker build -t bbc-scraper:1.1 .
 
 cd ../cnn-scraper
 npm install
-docker build -t cnn-scraper:1.0 .
+docker build -t cnn-scraper:1.1 .
 
-cd ../slashdot
+cd ../slashdot-scraper
 npm install
-docker build -t slashdot-scraper:1.0 .
+docker build -t slashdot-scraper:1.1 .
 
 cd ../data-store
 npm install
-docker build -t data-store:1.0 .
+docker build -t data-store:1.1 .
 
 cd ../id-service
 npm install
-docker build -t id-service:1.0 .
+docker build -t id-service:1.1 .
 
 cd ../translation-service
 npm install
-docker build -t translation-service:1.0 .
+docker build -t translation-service:1.1 .
