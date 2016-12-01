a3c54e291bdf8ebdc58fc92bc7d6cf83939b792d
M. Hofman <m.hofman@chronos-technology.nl>
2016-12-01 19:39:32 +0100

    add CORS

--- services/article-aggregate/run.js
+++ services/article-aggregate/run.js
@@ -46,3 +46,18 @@ function handleError( err, message ){
 
     return message;
 }
+
+http.addMiddleware(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Content-Length, X-Requested-With, apikey'
+    );
+
+    if(req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    next();
+});
+
