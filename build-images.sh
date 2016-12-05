c473cf9a99c0bd4ea112ccbcbd8a60d90f57dcaa
eva <squidstock@hotmail.com>
2016-12-05 16:47:53 +0100

    update image names; use docker hub instead of local cluster docker  storage

--- build-images.sh
+++ build-images.sh
@@ -5,33 +5,33 @@ set -e
 
 cd services/article-aggregate
 npm install
-docker build -t article-aggregate:1.1 .
+docker build -t meventures/demo-article-aggregate:1.1 .
 
 cd ../bbc-scraper
 npm install
-docker build -t bbc-scraper:1.1 .
+docker build -t meventures/demo-bbc-scraper:1.1 .
 
 cd ../cnn-scraper
 npm install
-docker build -t cnn-scraper:1.1 .
+docker build -t meventures/demo-cnn-scraper:1.1 .
 
 cd ../slashdot-scraper
 npm install
-docker build -t slashdot-scraper:1.1 .
+docker build -t meventures/demo-slashdot-scraper:1.1 .
 
 cd ../data-store
 npm install
-docker build -t data-store:1.1 .
+docker build -t meventures/demo-data-store:1.1 .
 
 cd ../id-service
 npm install
-docker build -t id-service:1.1 .
+docker build -t meventures/demo-id-service:1.1 .
 
 cd ../translation-service
 npm install
-docker build -t translation-service:1.1 .
+docker build -t meventures/demo-translation-service:1.1 .
 
 cd ../article-frontend
 npm install
 ng build --target=production --sourcemap=false
-docker build -t article-frontend:1.1 .
+docker build -t meventures/demo-article-frontend:1.1 .
